1f29cb8111789e8ab31707e0ce6442bf18cc96a1
AreebaAbid0067 <aareeba006@gmail.com>
2024-07-28 18:58:37 +0300

    changes saved

---
+++ src/mcqgenerator/MCQgenerator.py
@@ -0,0 +1,21 @@
+import os
+import json
+import traceback
+import pandas as pd
+
+from dotenv import load_dotenv
+
+
+from src.mcqgenerator.utils import read_file,get_table_data
+from src.mcqgenerator.logger import logging
+
+#importing necessary packages from langchain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+
+
+
+
+
